5b7eb8263d328545e5408ee0d50ecd12fb1002df
elmavalos <luismuaaf@gmail.com>
2024-10-20 21:49:17 -0600

    Commit de mi ecommerce

---
+++ src/components/CartIcon/CartIcon.jsx
@@ -0,0 +1,10 @@
+const CarritoIcon = () => {
+    return (
+        <a href="#" className="nav-link">
+            <i class="fa-solid fa-cart-shopping fa-bounce"></i>
+            <span>10</span>
+        </a>
+    )
+}
+
+export default CarritoIcon
